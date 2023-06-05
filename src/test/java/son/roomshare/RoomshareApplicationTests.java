1253bb6c0b0f17b2379523e3612d672de8b60151
Son won cheol <xzxz7003@naver.com>
2023-06-05 09:04:48 +0900

    <master>,원철 : setting

---
+++ src/test/java/son/roomshare/RoomshareApplicationTests.java
@@ -0,0 +1,13 @@
+package son.roomshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
