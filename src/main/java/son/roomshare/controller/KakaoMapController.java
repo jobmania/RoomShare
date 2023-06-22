ab99bf606d693659656169492122d48d9b5f8976
Son won cheol <xzxz7003@naver.com>
2023-06-22 12:13:31 +0900

    <feat> :  map 출력시 제한 ( 1000m 제한 설정 )
    - 출력 제한은 최대 10,20,30개 선택 가능하게
    - 출력 order는 거리순으로 최대한 가까운 순으로

--- src/main/java/son/roomshare/controller/KakaoMapController.java
+++ src/main/java/son/roomshare/controller/KakaoMapController.java
@@ -7,11 +7,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import son.roomshare.domain.map.MapRequest;
 import son.roomshare.domain.map.MapResponse;
 import son.roomshare.service.MapService;
 
+import javax.naming.Binding;
 import java.util.List;
 
 @Slf4j
@@ -23,16 +26,26 @@ public class KakaoMapController extends HomeController {
     private final MapService mapService;
 
     @GetMapping("")
-    public String viewPage(){
+    public String viewPage(@ModelAttribute MapRequest mapRequest){
         return "kakaomap/map";
     }
 
     @GetMapping("/food")
-    public String viewRestaurant(@ModelAttribute MapRequest mapRequest, Model model){
+    public String viewRestaurant(@Validated @ModelAttribute MapRequest mapRequest, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            log.error("bindingResult ={}",bindingResult.getTarget());
+
+            return "kakaomap/map";
+        }
+
 
         Double latitude = Double.parseDouble(mapRequest.getLatitude());
         Double longitude =  Double.parseDouble(mapRequest.getLongitude());
         Double inputDistance = Double.valueOf(mapRequest.getDistance());
+
+
+
 
        List<MapResponse> mapList =  mapService.getByDistance(latitude, longitude, inputDistance);
 
--- src/main/java/son/roomshare/domain/map/MapRequest.java
+++ src/main/java/son/roomshare/domain/map/MapRequest.java
@@ -2,11 +2,14 @@ package son.roomshare.domain.map;
 
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 @Builder
 public class MapRequest {
     private String latitude;
     private String longitude;
+
+    @Range(max = 1000)
     private Long distance;
 }
